c3952c23d5f9b323533cf695e0f0f9d81cd9c789
Ahmed Sameh <ahmedsaameh14@gmail.com>
2026-05-04 00:35:14 +0300

    Remove All Localhost:3000

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -7,47 +7,60 @@ import { IProductRes } from '../models/model';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  constructor(
+    private _http: HttpClient,
+    private _authS: AuthService,
+  ) {}
 
-  constructor(private _http:HttpClient , private _authS:AuthService) { }
-
-  URL = environment.API_URL + '/products/'
+  URL = environment.API_URL + '/products/';
   subCategoryURL = environment.API_URL + '/subcategory';
 
   getProducts(page: number = 1, limit: number = 6) {
-  return this._http.get<IProductsRes>(`${this.URL}?page=${page}&limit=${limit}`);
+    return this._http.get<IProductsRes>(
+      `${this.URL}?page=${page}&limit=${limit}`,
+    );
   }
 
-  getProductById(id:string){
-    return this._http.get<IProductRes>(this.URL + id)
+  getProductById(id: string) {
+    return this._http.get<IProductRes>(this.URL + id);
   }
 
-  getRelatedProducts(id:string){
-    return this._http.get<IProductRelatedRes>(this.URL + 'related/' + id)
+  getRelatedProducts(id: string) {
+    return this._http.get<IProductRelatedRes>(this.URL + 'related/' + id);
   }
 
   addProduct(formData: FormData) {
     const token = localStorage.getItem('token'); // adjust key if needed
-  const headers = new HttpHeaders({
-    Authorization: `Bearer ${token}`
-  });
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
     return this._http.post(`${this.URL}`, formData, { headers });
   }
 
   updateProduct(id: string, formData: FormData) {
-    return this._http.put(`${this.URL}${id}`, formData);
+    const token = localStorage.getItem('token');
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this._http.put(`${this.URL}${id}`, formData, { headers });
   }
 
-  getSubCategories(): Observable<{ message: string; data: { _id: string, name: string }[] }> {
-    return this._http.get<{ message: string; data: { _id: string, name: string }[] }>(this.subCategoryURL);
+  getSubCategories(): Observable<{
+    message: string;
+    data: { _id: string; name: string }[];
+  }> {
+    return this._http.get<{
+      message: string;
+      data: { _id: string; name: string }[];
+    }>(this.subCategoryURL);
   }
 
   addSubCategory(name: string) {
     return this._http.post(this.subCategoryURL, { name });
   }
-
-  
-
 }
